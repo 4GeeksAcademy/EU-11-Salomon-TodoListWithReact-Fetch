420c2953676f3285d6cabd368007d28ce1677997
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-06-21 19:24:56 +0000

    reactAndFetchLogicCompleted

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,75 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const createUser = async () => {
+        fetch ("https://assets.breatheco.de/apis/fake/todos/user/salo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify([])
+        }).then(res => {
+          return res.json()
+        }).then (data => {
+        console.log(data)
+      })
+    }
+    createUser()
+  }, []);
+
+  return (
+    <div>
+      <h1>todos</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <input
+                  type="text"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  value={inputValue}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      setTodos(todos.concat([inputValue]));
+                      setInputValue("");
+                    }
+                  }}
+                  placeholder="No tasks, add a task"
+                />
+              </li>
+              {todos.map((item, index) => (
+                <li
+                  key={index}
+                  style={{ display: "flex", alignItems: "center" }}
+                  className="list-group-item"
+                >
+                  <span style={{ flexGrow: 1 }}>{item}</span>
+                  <i
+                    className="fas fa-times"
+                    onClick={() =>
+                      setTodos(
+                        todos.filter((_, currentIndex) => index !== currentIndex)
+                      )
+                    }
+                  ></i>
+                </li>
+              ))}
+              <div className="todosLeft">
+                {todos.length} tasks left
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Home; 
